51c53ffdae5e1b1bbe408e62372c5359ab3ce232
gopimohan2002-git <74565994+gopimohan2002-git@users.noreply.github.com>
2020-11-17 02:48:54 +0530

    added values

--- src/main/java/com/git/action/GitActionDemoApplication.java
+++ src/main/java/com/git/action/GitActionDemoApplication.java
@@ -8,7 +8,7 @@ public class GitActionDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionDemoApplication.class, args);
-		System.out.println("Hello Demo:::");
+		System.out.println("Hello Demo:::Application for git Action");
 	}
 
 }
