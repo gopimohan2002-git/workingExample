6e27a72ac564356d496ee34c66ee2e2afb984c3b
gopimohan2002-git <74565994+gopimohan2002-git@users.noreply.github.com>
2020-11-23 19:39:16 +0530

    controller added

--- src/main/java/com/git/action/GitActionDemoApplication.java
+++ src/main/java/com/git/action/GitActionDemoApplication.java
@@ -2,8 +2,10 @@ package com.git.action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.git.**")
 public class GitActionDemoApplication {
 
 	public static void main(String[] args) {
